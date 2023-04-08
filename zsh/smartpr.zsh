325caba4a9dbf8132a36eb83ac8bb425cddfd809
Emiel van de Laar <gemiel@gmail.com>
2023-04-08 12:13:48 +0200

    latest dotfiles

---
+++ zsh/smartpr.zsh
@@ -0,0 +1,10 @@
+hash -d smartpr-api=~/Projects/smartpr-api
+hash -d smartpr-build=~/Projects/smartpr-build
+hash -d smartpr-client=~/Projects/smartpr-client
+hash -d smartpr-doc=~/Projects/smartpr-doc
+hash -d smartpr-store=~/Projects/smartpr-store
+hash -d smartpr-svcs=~/Projects/smartpr-svcs
+
+export PATH="/opt/homebrew/opt/node@14/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/node@14/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@14/include"
